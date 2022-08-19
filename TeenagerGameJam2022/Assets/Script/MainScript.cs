0dd0a3fa98a6d8c2b2fb17fbb8df0043c11ab988
konoyubin <choi82354@gmail.com>
2022-08-20 05:36:32 +0900

    Fix Script
    근데 이거 하나에 올빵하는게 맞나?

--- TeenagerGameJam2022/Assets/Script/MainScript.cs
+++ TeenagerGameJam2022/Assets/Script/MainScript.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,44 +10,39 @@ public class MainScript : MonoBehaviour
 {
     public enum Characteristic
     {
-        Observation, // 관찰력
-        Strong, //괴력
-        Sharpness, //날렵함
-        Intuition, //직감
-        Frostbite, //동상
-        f,
-        g,
-        h,
-        i,
-        j,
-        k,
-        l,
-        m,
-        n,
-        o,
-        p
+        Marksmanship, // 사격술
+        Strong, //근력
+        Sharpness, //민첩함    
+        Intuition, //생존본능
+        Meteorology, //기상학 v
+        Navigational, //항해력 v
+        Cooking, //요리력
+        Commanding, //통솔력 v
     }
     
     public enum Item
     {
         Match, //성냥
-        Meat, //고기
+        Gun, //총
+        Dog, //개  
         Jumper, // 방한복
         Medicine, //약
-        e,
-        f,
-        g,
+        Gold, //금화
+        Compass, // 나침반
     }
+    
     [SerializeField] private int hp;
     [SerializeField] private int mental;
-    [SerializeField] private int hunger;// 굳이 필요 한가? 한번 상의
 
-    private bool[] characteristicArray;
-    private bool[] itemArray;
+    private bool[] characteristicArray = new []{false, false, false, false, false, false, false, false};
+    private bool[] itemArray = new []{false, false, false, false, false, false, false};
     [SerializeField] private Text storyTeller;
     [SerializeField] private Text title;
+    [SerializeField] private Text statusText;
     private string storyScript;
+    private string profile;
     [SerializeField]private int mainStoryCount;
+    [SerializeField]private int mainStoryProgress;
 
     [SerializeField] private GameObject startButton;
     [SerializeField] private GameObject prologueButton1;
@@ -57,20 +53,24 @@ public class MainScript : MonoBehaviour
 
     private void Start()
     {
+        hp = 3;
+        mental = 3;
         mainStoryCount = 0;
+       // statusText.text = $"h: {hp} m: {mental}";
     }
 
     public void Prologue()
     {
+        //statusText.gameObject.SetActive(true);
         title.gameObject.SetActive(false);
         startButton.SetActive(false);
         storyTeller.gameObject.SetActive(true);
         storyScript =
-            " 1872년 7월 16일, 당신 어느 마을의  오두막에서 태어났습니다. \n" +
-            " 15살 시절 우연히 탐험 보고서를 얻은 후 읽으며 탐험가의 매력에 빠져 꿈을 키우기 시작했던 당신은 어느새 훌쩍 자라 첫 탐험으로 남극으로 떠났었죠. \n" +
-            " 펭귄과 바다표범을 먹은 적도 있었지만, 그건 중요하지 않을겁니다 \n" +
-            " 그 후 선장 면허를 취득한 당신은 자기학을 공부하고, 기상학 관련 지식을 배우며 누구와도 비교할 수 없는 만능 선장의 길을 걷게 되었습니다. \n" +
-            " 이후 북서항로도 정복한 당신은 남극점을 탐험하기 위하여 ‘프람 호’와 함께 여정을 준비하고 있네요. ";
+            "1872년 7월 16일, 당신은 어느 마을의 오두막에서 태어났습니다. " +
+            "15살 시절 우연히 탐험 보고서를 얻은 후 읽으며 탐험가의 매력에 빠져 꿈을 키우기 시작했던 당신은 어느새 훌쩍 자라 첫 탐험으로 남극으로 떠났었죠." +
+            " 펭귄과 바다표범을 먹은 적도 있었지만, 그건 중요하지 않을겁니다" +
+            " 그 후 선장 면허를 취득한 당신은 자기학을 공부하고, 기상학 관련 지식을 배우며 누구와도 비교할 수 없는 만능 선장의 길을 걷게 되었습니다." +
+            " 이후 북서항로도 정복한 당신은 남극점을 탐험하기 위하여 ‘프람 호’와 함께 여정을 준비하고 있네요.";
         StartCoroutine("TypingStory", storyScript);
         prologueButton1.SetActive(true);
     }
@@ -79,9 +79,10 @@ public class MainScript : MonoBehaviour
     {
         prologueButton1.SetActive(false);
         storyScript =
-            " 당신은 남극점을 정복하기 위한 모험을 떠나게 됩니다. \n" +
-            " 당신의 앞에는 위험이 도사릴지도 모르죠. 하지만, 그런 위험조차 당신의 압도적인 ‘이 능력’ 덕분에 헤쳐나갈 수 있겠죠! ";
+            "당신은 남극점을 정복하기 위한 모험을 떠나게 됩니다. " +
+            "당신의 앞에는 위험이 도사릴지도 모르죠. 하지만, 그런 위험조차 당신의 압도적인 ‘이 능력’ 덕분에 헤쳐나갈 수 있겠죠!";
         StartCoroutine("TypingStory", storyScript);
+        
         prologueButton2.SetActive(true);
     }
 
@@ -106,25 +107,31 @@ public class MainScript : MonoBehaviour
     public void GetCharacteristic1()
     {
         storyScript =
-            " 당신은 어려서부터 관찰력이 어쩌구 \n" +
-            " 어쩌구 저쩌구를 얻고 당신을 길을 떠났습니다..";
+            "당신은 어려서부터 호기심이 많았습니다 그것도 날씨에 관해서요!\n" +
+            "아마 남극에서 당신을 뛰어넘을 기상학자는 없을겁니다";
         StartCoroutine("TypingStory", storyScript);
+        Debug.Log((int)Characteristic.Meteorology);
+        characteristicArray[(int)Characteristic.Meteorology] = true;
         eventButton.SetActive(true);
     }
     public void GetCharacteristic2()
     {
         storyScript =
-            " 당신은 어려서부터 두려움이 어쩌구 \n" +
-            " 어쩌구 저쩌구를 얻고 당신을 길을 떠났습니다..";
+            "당신은 남극의 중심을 보기위해 정말 많은 노력을 해왔지만 그중에서도 항해실력은 으뜸이었습니다.\n" +
+            "당신의 항해 실력이면 남극까진 무사히 도착할것 같군요.";
         StartCoroutine("TypingStory", storyScript);
+        Debug.Log((int)Characteristic.Navigational);
+        characteristicArray[(int)Characteristic.Navigational] = true;
         eventButton.SetActive(true);
     }
     public void GetCharacteristic3()
     {
         storyScript =
-            " 당신은 어려서부터 직감이 어쩌구 \n" +
-            " 어쩌구 저쩌구를 얻고 당신을 길을 떠났습니다..";
+            "당신은 동네에서 놀때에도 모두를 한데 뭉치는데 능숙했죠.\n" +
+            "여렸을때부터 갈고닦아온 통솔력의 힘을 보여줄때가 왔습니다!";
         StartCoroutine("TypingStory", storyScript);
+        Debug.Log((int)Characteristic.Commanding);
+        characteristicArray[(int)Characteristic.Commanding] = true;
         eventButton.SetActive(true);
     }
 
@@ -135,24 +142,23 @@ public class MainScript : MonoBehaviour
             HpEnding();
         else if (mental <= 0)
             MentalEnding();
-        else if (mainStoryCount == 5 || mainStoryCount == 10 || mainStoryCount == 15 || mainStoryCount == 20 ||
-                 mainStoryCount == 25)
+        else if (mainStoryCount > 5 && 1 == Random.Range(0,2))
         {
-            switch (mainStoryCount)
+            switch (mainStoryProgress)
             {
-                case 5:
+                case 0:
                     MainStory1();
                     break;
-                case 10:
+                case 1:
                     MainStory2();
                     break;
-                case 15:
+                case 2:
                     MainStory3();
                     break;
-                case 20:
+                case 3:
                     MainStory4();
                     break;
-                case 25:
+                case 4:
                     MainStory5();
                     break;
             }
